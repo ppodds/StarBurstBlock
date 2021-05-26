253e12bbf122532c7d2741e3338a18732f4386ed
ppodds <oscar20020629@gmail.com>
2021-05-26 12:15:08 +0800

    修正GarbageBlock待在頂端卻不會結束遊戲的問題

--- src/main/java/org/ppodds/core/game/boss/GarbageBlock.java
+++ src/main/java/org/ppodds/core/game/boss/GarbageBlock.java
@@ -10,12 +10,12 @@ import org.ppodds.core.game.Position;
 import org.ppodds.core.game.Tetris;
 import org.ppodds.core.game.ui.GamePane;
 
-public class GarbageBlock {
+public class GarbageBlock { // TODO 看來 GarbageBlock也要改成 Group 的形式，否則掉下的先後順序會產生錯誤
     private final Tetris game;
     private final Pane block;
     private Position position;
     private final Timeline dropAnimation;
-    private boolean animationFinished = false;
+    private boolean animationFinished = false; // TODO 如果玩家的方塊在 Boss 方塊落下時剛好在方塊要落下的位置也會產生錯誤。
 
     public boolean isAnimationFinished() {
         return animationFinished;
@@ -32,30 +32,33 @@ public class GarbageBlock {
         GamePane.setRowIndexByCenterY(this.block, position.y);
         dropAnimation = new Timeline(new KeyFrame(Duration.millis(50), e -> {
             Position positionNow = getPosition();
-            if (positionNow.y < Tetris.boardHeight-1) {
-                if (game.getBlockOnBoard(positionNow.x, positionNow.y+1) != null) {
-                    set();
-                }
-                else {
-                    down();
-                }
-            }
-            else
+            if (positionNow.y + 1 == Tetris.boardHeight) {
                 set();
+            }
+            if (game.getBlockOnBoard(positionNow.x, positionNow.y+1) != null) {
+                set();
+            }
+            else {
+                down();
+            }
         }));
         dropAnimation.setCycleCount(Timeline.INDEFINITE);
         dropAnimation.play();
     }
     // TODO 不知道為甚麼，Boss丟下來的方塊在顯示效果和碰撞上有問題，待修正
     private void set() {
-        game.setBlockOnBoard(this.block, this.position.x, this.position.y);
+        if (position.y < 0)
+            game.gameOver(false);
+        else {
+            game.setBlockOnBoard(this.block, this.position.x, this.position.y);
+            game.getBoss().damage(game.eliminate(), null);
+        }
         dropAnimation.stop();
         animationFinished = true;
-        game.getBoss().damage(game.eliminate(), null);
     }
 
     private void down() {
-        position = new Position(position.x, position.y+1);
+        position.y++;
         GamePane.setRowIndexByCenterY(this.block, position.y);
     }
 
